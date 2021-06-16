6d52628d9abd734b20dd21c1ca093ad6db353301
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-06-17 00:20:08 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPriceInKop = 20000_00;
+        int oneMillinBonus = 20;
+        int resultMillBonus = ticketPriceInKop/oneMillinBonus;
+        System.out.println(resultMillBonus);
+    }
+}
