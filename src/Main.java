74b3a8c7857ed71a437878a8c068b11c8f3df984
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-06-18 22:19:30 +0300

    correct version

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPriceInKop = 20000_00;
-        int oneMillinBonus = 20;
+        int oneMillinBonus = 20_00;
         int resultMillBonus = ticketPriceInKop/oneMillinBonus;
         System.out.println(resultMillBonus);
     }
